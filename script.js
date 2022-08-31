633001225dac4170af18bb06a4758452e9d1da78
Hamza Amar <rhamzax@gmail.com>
2022-08-30 22:58:22 -0600

    Create an Api call for a random pokemon
    Co-authored-by: Akaash Aujla <AkaashAujla@users.noreply.github.com>

--- script.js
+++ script.js
@@ -22,17 +22,33 @@ function createEntry(){
     mainGrid.appendChild(gridEntry);
 }
 
-function createTop(){
 
-}
-
-function createImg(){
-
-}
 function createPokedex(){
     for(let i = 0; i < 900; i++){
         createEntry();
     }
     
+}
+
+function ApiRequest(randomPokemon){
+    let urlGen = `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`
+    const pokeimg = document.querySelector(".main-img")
+    
+    fetch(urlGen)
+      .then(response => {
+        // indicates whether the response is successful (status code 200-299) or not
+        if (!response.ok) {
+          throw new Error(`Request failed with status ${response.status}`)
+        }
+        return response.json()
+      })
+      .then(data => {
+        pokeName = data.species.name
+        pokeName = pokeName.replace("-", " ")
+        errorBox.textContent = ""
+        // document.querySelector(".name").textContent = data.name
+        document.querySelector(".main-img").src = data.sprites.front_default
+      })
+      .catch(error => console.log(error))
 }
 createPokedex();
