c5415dcf0ac11257c0c974373413579edb3ddb63
Hamza Amar <rhamzax@gmail.com>
2022-09-02 00:17:51 -0600

    Create Modal dynamically for each pokemon
    Co-authored-by: Arshvir Singh <arshvirsingh16@users.noreply.github.com>

--- pokemon.js
+++ pokemon.js
@@ -6,16 +6,34 @@ class Pokemon{
     setData(data){
         this.data = data;
         this.setName();
+        this.setStats();
+        this.setCharisteristic();
     }
 
     setName(){
         this.pokeName = this.data.species.name
     }
 
+    setCharisteristic(){
+        this.height = this.data.height/10
+        this.weight = this.data.weight/10
+        this.abilities = this.data.abilities
+    }
+
+    setStats(){
+        this.hp = this.data.stats[0].base_stat
+        this.attack = this.data.stats[1].base_stat
+        this.defense = this.data.stats[2].base_stat
+        this.spAttack = this.data.stats[3].base_stat
+        this.spDefense = this.data.stats[4].base_stat
+        this.speed = this.data.stats[5].base_stat
+        this.total = this.hp + this.attack + this.defense + this.spAttack + this.spDefense + this.speed
+    }
+
     createEntry(){
         const gridEntry = document.createElement('div');
-        gridEntry.addEventListener('click', showInfo)
         gridEntry.className = 'entry';
+        gridEntry.id = `${this.index}`
         gridEntry.innerHTML = 
         `
         <div class="top">
@@ -32,8 +50,68 @@ class Pokemon{
         </div>
         
         `
+        gridEntry.addEventListener('click', showInfo)
+        console.log(this.hp)
         return gridEntry;
     }
-
-    
+    createModal(){
+        const modalContent = document.createElement('div');
+        modalContent.className = 'pokeModalContent'
+        modalContent.innerHTML =
+        `
+            <span class="close" id="left">&times;</span>
+            <h3 class="pokeName">${this.pokeName} #${this.index}</h3>
+            <div class="pokeImg">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.index}.png" alt="Pokemon" class="pokeImg">
+            </div>
+            <div class="pokeCharacteristic">
+                <p class="CharacteristicTitle">Type</p>
+                <div class="pokeTypes">
+                    <p>Fire</p>
+                    <p>Flying</p>
+                </div>
+                <p class="CharacteristicTitle">Height</p>
+                <p>${this.height}m ( 1'12" )</p>
+                <p class="CharacteristicTitle">Weight</p>
+                <p>${this.weight}kg ( 18.7lbs. )</p>
+                <p class="CharacteristicTitle">Abilities</p>
+                <div class="pokeAbilities">
+                    <p>Blaze</p>
+                    <p>Solar-Power</p>    
+                    <p>Solar-Power</p> 
+                </div>
+            </div>
+            <p class="StatisticTitle">Stats</p>
+            <div class="stats">
+                <p class="stat-title">Hp</p>
+                <div class="animated-progress progress-blue">
+                    <span data-progress="45" style="width: ${this.hp}%;">${this.hp}</span>
+                </div>    
+                <p class="stat-title">Attack</p>
+                <div class="animated-progress progress-blue">
+                    <span data-progress="45" style="width: ${this.attack}%;">${this.attack}</span>
+                </div>    
+                <p class="stat-title">Defense</p>
+                <div class="animated-progress progress-blue">
+                    <span data-progress="45" style="width: ${this.defense}%;">${this.defense}</span>
+                </div>    
+                <p class="stat-title">Sp. Attack</p>
+                <div class="animated-progress progress-blue">
+                    <span data-progress="45" style="width: ${this.spAttack}%;">${this.spAttack}</span>
+                </div>    
+                <p class="stat-title">Sp. Defense</p>
+                <div class="animated-progress progress-blue">
+                    <span data-progress="45" style="width: ${this.spDefense}%;">${this.spDefense}</span>
+                </div>    
+                <p class="stat-title">Speed</p>
+                <div class="animated-progress progress-blue">
+                    <span data-progress="45" style="width: ${this.speed}%;">${this.speed}</span>
+                </div>    
+                <p class="stat-title">Total</p>
+                <p>${this.total}</p>   
+                 
+            </div>
+        `
+        return modalContent
+    }
 }
--- script.js
+++ script.js
@@ -1,62 +1,20 @@
 const mainGrid = document.querySelector('main');
 const modal = document.querySelector(".pokeModal");
-const btn = document.querySelector(".btn");
-const span = document.querySelector(".close");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-function createEntry(index, pokeName){
-    const gridEntry = document.createElement('div');
-    gridEntry.addEventListener('click', showInfo)
-    gridEntry.className = 'entry';
-    gridEntry.innerHTML = 
-    `
-    <div class="top">
-        <div class="left">
-            <h2 class="pokeID">${index}</h2>
-            <h3 class="pokeName">${pokeName}</h3>
-        </div>
-        <div class="right">
-            <h3 class="https://pokeapi.co/api/v2/type/13/"></h3>
-        </div>
-    </div>
-    <div class="pokeImg">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png" alt="Pokemon" class="pokeImg">
-    </div>
-    
-    `
-    mainGrid.appendChild(gridEntry);
-}
+} 
 
-function showInfo(){
-    console.log("1")
-    
-}
 let pokedex = []
 async function createPokedex(){
-    for(let i = 1; i < 899; i++){
+    for(let i = 1; i < 200; i++){
       let pokemon = new Pokemon(i)
-      // ApiRequest(i).then(data => {
-      //   // console.log(data)
-      //   pokemon.setData(data);
-      //   console.log(pokemon.data)
-      // }).catch(err => {
-      //   console.log(err);
-      // });
       pokemon.setData(await ApiRequest(i))
       pokedex.push(pokemon)
       mainGrid.appendChild(pokemon.createEntry());
@@ -64,8 +22,22 @@ async function createPokedex(){
     }
     
 }
+function showInfo(){
+  let pokemon = pokedex[this.id-1]  
+  modal.innerHTML = ''
+  console.log(pokemon.createModal())
+  modal.appendChild(pokemon.createModal())
+  modal.style.display = "block";
+  enableCloseBtn();
+  
+}
 
-
+function enableCloseBtn(){
+  const span = document.querySelector(".close");
+  span.onclick = function() {
+    modal.style.display = "none";
+  }   
+}
 /* $(".animated-progress span").each(function () {
     $(this).animate(
       {
